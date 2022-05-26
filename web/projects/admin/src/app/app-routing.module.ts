1ac45dc8e4e7a7656efa06a4784104a48056ebd8
Alva <asjun2lin@outlook.com>
2022-05-26 23:54:37 +0800

    add 添加登录页面

--- web/projects/admin/src/app/app-routing.module.ts
+++ web/projects/admin/src/app/app-routing.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   {
-    path: 'welcome',
-    loadChildren: () =>
-      import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      {
+        path: 'welcome',
+        loadChildren: () =>
+          import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
+      },
+      {
+        path: 'article',
+        loadChildren: () =>
+          import('./pages/article/article.module').then((m) => m.ArticleModule),
+      },
+    ],
   },
   {
-    path: 'article',
-    loadChildren: () =>
-      import('./pages/article/article.module').then((m) => m.ArticleModule),
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
--- web/projects/admin/src/app/app.module.ts
+++ web/projects/admin/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { NZ_I18N, zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
@@ -14,11 +14,19 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { environment } from '../environments/environment';
 import { AppConfig, LentinulaLibModule } from 'lentinula-lib';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 registerLocaleData(zh);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -29,6 +37,13 @@ registerLocaleData(zh);
     NzLayoutModule,
     NzMenuModule,
     LentinulaLibModule,
+    NzInputModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzCheckboxModule,
+    NzButtonModule,
+    NzCardModule,
+    NzDividerModule,
   ],
   providers: [
     { provide: NZ_I18N, useValue: zh_CN },
