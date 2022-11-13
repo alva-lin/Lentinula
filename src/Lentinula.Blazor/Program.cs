a3550a44de33a790fe01768f25d31e63871751de
Alva <asjun2lin@outlook.com>
2022-11-14 00:00:09 +0800

    更新 gitignore, 引入 tailwind 配置

--- src/Lentinula.Blazor/Program.cs
+++ src/Lentinula.Blazor/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using Lentinula.Blazor.Wasm;
+using Lentinula.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
---
+++ src/Lentinula.Blazor/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{razor,html,cshtml,cs,css}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
