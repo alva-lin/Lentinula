dfec565fd63adb311990829429ae77409d56fd39
Alva <asjun2lin@outlook.com>
2022-11-23 00:39:19 +0800

    转换 markdown 格式的文本

---
+++ src/Lentinula.Blazor/wwwroot/js/article.js
@@ -0,0 +1,16 @@
+﻿export function readFile() {
+    for(let i = 1; i < 6; i++) {
+        document.getElementById(`btn${i}`).addEventListener('click', function() {
+            fetch(`files/0${i}.md`)
+                .then(async response => {
+                    const text = await response.text();
+                    // console.log(text)
+                    const clean = DOMPurify.sanitize(text);
+                    // console.log(clean);
+                    // document.getElementById('article').textContent = clean;
+                    const html = marked.parse(clean);
+                    document.getElementById('article').innerHTML = html;
+                });
+        })
+    }
+}
