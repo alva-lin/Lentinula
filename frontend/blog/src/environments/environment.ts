302383d1b956bf3f8ea17a91e069a04663189b8a
Alva <asjun2lin@outlook.com>
2022-12-12 00:23:34 +0800

    设置页面底部的备案号

--- frontend/blog/src/environments/environment.ts
+++ frontend/blog/src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // apiUrl: "http://localhost:39001"
+  apiUrl: "api",
+  beian: '测ICP备xxxxxxxxxx号',
 };
 
 /*
